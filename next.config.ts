e8316e1a5487f1b5f40b5da7a05d945c5cc0710f
cruzbeymar74 <cruzbeymar74@gmail.com>
2026-03-27 01:42:17 +0000

    arreglos logo

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,3 @@
 import type { NextConfig } from "next";
-const nextConfig: NextConfig = {
-  serverExternalPackages: ["@vercel/og"],
-};
+const nextConfig: NextConfig = {};
 export default nextConfig;
-import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
