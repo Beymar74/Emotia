bd2adba909a3e7fe4b929216910ff81f84de5a90
cruzbeymar74 <cruzbeymar74@gmail.com>
2026-03-27 01:33:45 +0000

    arreglos logo

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
-  experimental: {
-    optimizePackageImports: ["@vercel/og"],
-  },
   serverExternalPackages: ["@vercel/og"],
 };
 export default nextConfig;
